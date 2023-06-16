bd53652f14e3adcd3bc6fa7c84b9e364554925c3
GabrielaMitu <gabrielaym@al.insper.edu.br>
2023-06-16 12:54:21 -0300

    countdiamonds

---
+++ Assets/Scripts/countDiamond.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countDiamond : MonoBehaviour
+{
+    public static countDiamond instance; // Referência estática para o GameController
+
+    private int diamantes = 0; // Variável para contar os diamantes
+
+    private void Awake()
+    {
+        // Garante que apenas um GameController exista na cena
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Método para adicionar diamantes à contagem global
+    public void GanharDiamante()
+    {
+        diamantes++;
+    }
+
+    // Método para obter a contagem atual de diamantes
+    public int ObterContagemDiamantes()
+    {
+        return diamantes;
+    }
+}
